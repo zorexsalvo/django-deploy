ca9a3999394f26e94c8de1925baa566ad3a7325a
Zorex Salvo <zorexsalvo@gmail.com>
2018-04-02 22:26:39 +0800

    Create endpoint for listing/creating user

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from api.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows user to be viewed
+    and edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
